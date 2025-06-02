8c201914242507234b327f19d4ab589763e35e54
Gabrielle-Byrne <156119029+Gabrielle-Byrne@users.noreply.github.com>
2025-06-02 14:31:14 -0300

    Vercel Fixes
    Removes routes and fixes some vercel issues

---
+++ front-end/src/app/resources-editor-legal-clinic-unb/formHelper.js
@@ -0,0 +1,58 @@
+// intake-editor/formHelper.js
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+const cors = require('cors');
+
+const allowedOrigin = 'http://localhost:3001/eligibility';
+
+export async function saveFormData(formData) {
+  try {
+    const { _id, ...safeData } = formData; 
+    console.log('Saving form data:', safeData);
+    const response = await fetch(allowedOrigin, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(safeData),
+    });
+
+    const result = await response.json();
+    console.log('Insert result:', result);
+    if (!response.ok) {
+      throw new Error(result.error || 'Failed to save data');
+    }
+
+    return { success: true, data: result };
+  } catch (error) {
+    console.error('Error saving form data (HELLO):', error);
+    return { success: false, error: error.message };
+  }
+}
+
+export async function getFormData() {
+  try {
+    const response = await fetch('http://localhost:3001/eligibility');
+    const result = await response.json();
+    
+    if (!response.ok) {
+      throw new Error(result.error || 'Failed to load data');
+    }
+
+    return { success: true, data: result };
+  } catch (error) {
+    console.error('Error loading form data:', error);
+    return { success: false, error: error.message };
+  }
+
+  
+}
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': allowedOrigin,
+    },
+  })
+}
