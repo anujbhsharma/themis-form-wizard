d20ecb5a7234920aabce1a95b3712a1e3a0340a1
Gabrielle-Byrne <156119029+Gabrielle-Byrne@users.noreply.github.com>
2025-06-03 10:34:58 -0300

    Update route.js

--- front-end/src/app/server/route.js
+++ front-end/src/app/server/route.js
@@ -7,48 +7,48 @@ const app = express.Router();
 app.use(cors());
 app.use(express.json());
 
-/**
- * POST to eligibility collection
- */
-app.post('/eligibility', async (req, res) => {
-  const db = await connectDB();
-  const result = await db.collection('eligibility').insertOne(req.body);
-  res.json({ insertedId: result.insertedId });
-});
+// /**
+//  * POST to eligibility collection
+//  */
+// app.post('/eligibility', async (req, res) => {
+//   const db = await connectDB();
+//   const result = await db.collection('eligibility').insertOne(req.body);
+//   res.json({ insertedId: result.insertedId });
+// });
 
-/**
- * POST to intake collection
- */
-app.post('/intake', async (req, res) => {
-  const db = await connectDB();
-  const result = await db.collection('intake').insertOne(req.body);
-  res.json({ insertedId: result.insertedId });
-});
+// /**
+//  * POST to intake collection
+//  */
+// app.post('/intake', async (req, res) => {
+//   const db = await connectDB();
+//   const result = await db.collection('intake').insertOne(req.body);
+//   res.json({ insertedId: result.insertedId });
+// });
 
-/**
- * GET all eligibility
- */
-app.get('/eligibility', async (req, res) => {
-  const db = await connectDB();
-  const eligibilities = await db.collection('eligibility')
-    .find()
-    .sort({ _id: -1 })
-    .limit(1)
-    .toArray();
+// /**
+//  * GET all eligibility
+//  */
+// app.get('/eligibility', async (req, res) => {
+//   const db = await connectDB();
+//   const eligibilities = await db.collection('eligibility')
+//     .find()
+//     .sort({ _id: -1 })
+//     .limit(1)
+//     .toArray();
 
-  res.json(eligibilities[0] || {}); // Return empty object if none found
-});
+//   res.json(eligibilities[0] || {}); // Return empty object if none found
+// });
 
-/**
- * GET all intake
- */
-app.get('/intake', async (req, res) => {
-  const db = await connectDB();
-  const intakes = await db.collection('intake').find()
-    .sort({ _id: -1 })
-    .limit(1)
-    .toArray();
-  res.json(intakes);
-});
+// /**
+//  * GET all intake
+//  */
+// app.get('/intake', async (req, res) => {
+//   const db = await connectDB();
+//   const intakes = await db.collection('intake').find()
+//     .sort({ _id: -1 })
+//     .limit(1)
+//     .toArray();
+//   res.json(intakes);
+// });
 
 module.exports = app; // export router
