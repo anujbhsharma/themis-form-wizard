31813472309116b14eb9a97bdb00c560696c60ea
Gabrielle-Byrne <156119029+Gabrielle-Byrne@users.noreply.github.com>
2025-06-03 08:40:05 -0300

    Test 1

---
+++ front-end/src/app/server/config/db.js
@@ -0,0 +1,13 @@
+// const { MongoClient } = require('mongodb');
+// require('dotenv').config();
+
+// const client = new MongoClient(process.env.MONGO_URI);
+
+// async function connectDB() {
+//   if (!client.topology || !client.topology.isConnected()) {
+//     await client.connect();
+//   }
+//   return client.db(process.env.DB_NAME);
+// }
+
+// module.exports = connectDB;
